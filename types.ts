2403c72971e419a968205d261c7362e942c4408f
Blake Seipler <bseipler@yahoo.com>
2022-08-13 21:47:26 -0400

    add Classroom type

---
+++ types.ts
@@ -0,0 +1,10 @@
+export interface Classroom {
+  id: number;
+  created_at: string;
+  category: string;
+  ratio: string;
+  teachers: number;
+  enrolled: number;
+  capacity: number;
+  checked_in: number;
+}
