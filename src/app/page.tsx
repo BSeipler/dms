ee004482b35e670d223e91a6218a290d5716172a
Blake Seipler <bseipler@yahoo.com>
2023-03-26 12:10:47 -0400

    chore: fixed indentation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div>
-       <h1 className="p-6">My name is Josh</h1>
-       <h1>I like puppies!</h1>
+      <h1 className="p-6">My name is Josh</h1>
+      <h1>I like puppies!</h1>
     </div>
   );
 }
